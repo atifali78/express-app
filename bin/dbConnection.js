11b0921fea471473fd182809e5f338b2337d45d9
atifali78 <atif7803320@gmail.com>
2024-08-13 13:02:40 +0500

    reomve errors from validations

--- bin/dbConnection.js
+++ bin/dbConnection.js
@@ -5,13 +5,24 @@ const sequelize = new Sequelize("mr19", "postgres", "4136806", {
   dialect: "postgres",
 });
 
-sequelize
-  .authenticate()
-  .than(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((error) => {
-    console.error(error.message);
-  });
+// sequelize
+//   .authenticate()
+//   .than(() => {
+//     console.log("Connection has been established successfully.");
+//   })
+//   .catch((error) => {
+//     console.error(error.message);
+//   });
+const connectDb = async() => {
+    
+    try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+};
 
-module.export = sequelize;
+
+module.exports = sequelize;
+module.exports ={connectDb}
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
 var routes = require("express").Router();
 const { create, getAll } = require("../controllers/userController");
-const { createUserSchema } = require("../validation/users");
+const { createUserSchema } = require("../validations/user");
 routes.get("/get-all-users", getAll);
 routes.post("/create-user", createUserSchema, create);
 
